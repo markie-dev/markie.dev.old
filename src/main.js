160d526cfcab83bcbe9d30d5a7def5bfdab798de
marcus <markie@markie.dev>
2024-05-16 23:49:46 -0500

    adjusted scrolling threshold

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 let lastScrollTop = 0;
 const banner = document.querySelector('.banner');
+const threshold = 50;
 
 window.addEventListener('scroll', function() {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   let scrollHeight = document.documentElement.scrollHeight;
   let clientHeight = document.documentElement.clientHeight;
 
-  if (scrollTop > lastScrollTop && scrollTop > 0){
-    // Downscroll, not at the top of the page
+  if (scrollTop > lastScrollTop && scrollTop > threshold){
     banner.classList.add('hide');
   } else {
-    // Upscroll or at the top of the page
-    // Check if user is at the bottom of the page
     if (scrollTop + clientHeight < scrollHeight) {
       banner.classList.remove('hide');
     }
