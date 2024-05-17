682c98d10f4b794be88011a8b02982a993dcbc20
marcus <markie@markie.dev>
2024-05-17 14:27:53 -0500

    fixed about page links

---
+++ src/about.js
@@ -0,0 +1,22 @@
+let lastScrollTop = 0;
+let isScrolling;
+const banner = document.querySelector('.banner');
+const threshold = 50;
+
+window.addEventListener('scroll', function() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight;
+  let clientHeight = document.documentElement.clientHeight;
+
+  if (scrollTop > lastScrollTop && scrollTop > threshold){
+    banner.classList.add('hide');
+  } else {
+    if (scrollTop + clientHeight < scrollHeight) {
+      window.clearTimeout(isScrolling);
+      isScrolling = setTimeout(function() {
+        banner.classList.remove('hide');
+      }, 5);
+    }
+  }
+  lastScrollTop = scrollTop;
+});
